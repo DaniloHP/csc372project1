8a942af6a085153308391c6ac67262b867044270
Danilo Hidalgo-Putnam <danilohidalgo@email.arizona.edu>
2021-09-08 12:27:35 -0700

    hello world achieved

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+struct Repo {
+    id: int32,
+    node_id: String,
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
