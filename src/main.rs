00b541fb0646f06eee77fe52aef5dbb10e60d72f
Danilo Hidalgo-Putnam <danilohidalgo@email.arizona.edu>
2021-09-08 12:33:20 -0700

    remove garbage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use std::collections::HashMap;
 
-struct Repo {
-    id: int32,
-    node_id: String,
-    name: String,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://httpbin.org/ip")
