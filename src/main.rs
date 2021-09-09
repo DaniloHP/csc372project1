140deb6e86859e4fe76a2a7946783e74511c64f3
Danilo Hidalgo-Putnam <danilohidalgo@email.arizona.edu>
2021-09-08 19:21:50 -0700

    first fetch of user repos, add structer to project

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::collections::HashMap;
+mod types;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .get("https://api.github.com/users/DaniloHP/repos")
+        .header("User-agent", "")
+        .send()?
+        .json::<Vec<types::repo::Repo>>()?;
     println!("{:#?}", resp);
     Ok(())
 }
---
+++ structer/src/structer.js
@@ -0,0 +1,84 @@
+import fetch from "node-fetch";
+
+const args = process.argv;
+const endpoint = args.length > 2 ? args[2] : "users/DaniloHP/repos";
+const url = `https://api.github.com/${endpoint}`;
+fetch(url).then((res) =>
+  res.json().then((j) => {
+    console.log("use serde::Deserialize;")
+    Array.isArray(j) ? iterObj(j[0], "Repo") : iterObj(j, "Repo");
+    // if it's an array, only call on the first item
+  })
+);
+
+const toRustType = (val) => {
+  if (val === null) return "Option<String>"; // not perfect
+  switch (typeof val) {
+    case "number":
+      return "i32";
+    case "string":
+      return "Option<String>";
+    case "boolean":
+      return "bool";
+    default:
+      return "";
+  }
+};
+
+const iterObj = (obj, structName) => {
+  let soFar = "";
+  soFar += `\n#[derive(Debug, Deserialize)]\npub struct ${structName} {\n`;
+  Object.entries(obj).forEach((e) => {
+    const [key, val] = e;
+    const pref = keywords.has(key) ? " // " : "    ";
+    // prefix with a comment if a key has a name that is reserved in Rust
+    if (val && typeof val === "object") {
+      const tcStruct = key[0].toUpperCase() + key.substring(1);
+      // ^ Titlecase, as Rust likes its struct names
+      soFar += `${pref}pub ${key}: Option<${tcStruct}>,\n`;
+      iterObj(obj[key], tcStruct);
+    } else {
+      soFar += `${pref}pub ${key}: ${toRustType(val)},\n`;
+    }
+  });
+  soFar = soFar.substring(0, soFar.length - 2) + "\n}";
+  console.log(soFar);
+};
+
+const keywords = new Set([
+  "as",
+  "break",
+  "const",
+  "continue",
+  "crate",
+  "else",
+  "enum",
+  "extern",
+  "false",
+  "fn",
+  "for",
+  "if",
+  "impl",
+  "in",
+  "let",
+  "loop",
+  "match",
+  "mod",
+  "move",
+  "mut",
+  "pub",
+  "ref",
+  "return",
+  "self",
+  "Self",
+  "static",
+  "struct",
+  "super",
+  "trait",
+  "true",
+  "type",
+  "unsafe",
+  "use",
+  "where",
+  "while",
+]);
